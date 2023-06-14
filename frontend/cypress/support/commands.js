cd0ccfefb7c76f5f6bcfb1acc8d08a80c0f5e0a7
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-06-14 13:29:54 +0100

    Removed redundant Cypress files

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,15 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
 Cypress.Commands.add('signup', (email, username, password) => {
   cy.visit("/signup");
   cy.get("#email").type(email);
@@ -21,15 +9,4 @@ Cypress.Commands.add('signup', (email, username, password) => {
 Cypress.Commands.add('testCleanup', () => {
   cy.task('clearDB')
 })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
